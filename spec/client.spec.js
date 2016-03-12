2fc39df03ed7d0a17bf5568fd9715d0e39ce5220
David Neal <david@reverentgeek.com>
2016-03-12 13:52:12 -0600

    adding languages and versions, refactored method names

--- spec/client.spec.js
+++ spec/client.spec.js
@@ -1,6 +1,7 @@
 import Client from "../src/client";
 
-describe( "Client Specs", () => {
+describe( "Client Specs", function() {
+	this.timeout( 10000 );
 	const apiKey = process.env["DBP_API_KEY"];
 	let runIntegrationSpecs = !!apiKey;
 
@@ -14,8 +15,50 @@ describe( "Client Specs", () => {
 				client = new Client( apiKey );
 			} );
 
-			it( "volumeLanguageFamilyList should get a list of language families", ( done ) => {
-				client.volumeLanguageFamilyList( ( err, res ) => {
+			it( "languages should get a list of languages", ( done ) => {
+				client.languages( ( err, res ) => {
+					should.not.exist( err );
+					should.exist( res );
+					// console.log( res );
+					res.should.be.instanceOf( Array );
+					res.length.should.be.above( 0 );
+					let language = res[ 0 ];
+					language.should.have.property( "language_code" );
+					language.should.have.property( "language_name" );
+					language.should.have.property( "english_name" );
+					language.should.have.property( "language_iso" );
+					language.should.have.property( "language_iso_2B" );
+					language.should.have.property( "language_iso_2T" );
+					language.should.have.property( "language_iso_1" );
+					language.should.have.property( "language_iso_name" );
+					language.should.have.property( "language_family_code" );
+					done();
+				} );
+			} );
+
+			it( "languages should get a list of languages by language code", ( done ) => {
+				client.languages( { code: "ENG" }, ( err, res ) => {
+					should.not.exist( err );
+					should.exist( res );
+					// console.log( res );
+					res.should.be.instanceOf( Array );
+					res.length.should.be.equal( 1 );
+					let language = res[ 0 ];
+					language.should.have.property( "language_code" );
+					language.should.have.property( "language_name" );
+					language.should.have.property( "english_name" );
+					language.should.have.property( "language_iso" );
+					language.should.have.property( "language_iso_2B" );
+					language.should.have.property( "language_iso_2T" );
+					language.should.have.property( "language_iso_1" );
+					language.should.have.property( "language_iso_name" );
+					language.should.have.property( "language_family_code" );
+					done();
+				} );
+			} );
+
+			it( "volumeLanguageFamilies should get a list of language families", ( done ) => {
+				client.volumeLanguageFamilies( ( err, res ) => {
 					should.not.exist( err );
 					should.exist( res );
 					// console.log( res );
@@ -35,9 +78,54 @@ describe( "Client Specs", () => {
 				} );
 			} );
 
-			it( "volumeLanguageFamilyList should get a list of language families by media", ( done ) => {
+			it( "versions should get a list of versions", ( done ) => {
+				client.versions( ( err, res ) => {
+					should.not.exist( err );
+					should.exist( res );
+					// console.log( res );
+					res.should.be.instanceOf( Array );
+					res.length.should.be.above( 1 );
+					let version = res[ 0 ];
+					version.should.have.property( "version_code" );
+					version.should.have.property( "version_name" );
+					version.should.have.property( "english_name" );
+					done();
+				} );
+			} );
+
+			it( "versions should get a list of versions by code", ( done ) => {
+				client.versions( { code: "ESV" }, ( err, res ) => {
+					should.not.exist( err );
+					should.exist( res );
+					// console.log( res );
+					res.should.be.instanceOf( Array );
+					res.length.should.be.equal( 1 );
+					let version = res[ 0 ];
+					version.should.have.property( "version_code" ).that.is.equal( "ESV" );
+					version.should.have.property( "version_name" );
+					version.should.have.property( "english_name" );
+					done();
+				} );
+			} );
+
+			it( "versions should get a list of versions by name", ( done ) => {
+				client.versions( { name: "English" }, ( err, res ) => {
+					should.not.exist( err );
+					should.exist( res );
+					// console.log( res );
+					res.should.be.instanceOf( Array );
+					res.length.should.be.above( 1 );
+					let version = res[ 0 ];
+					version.should.have.property( "version_code" );
+					version.should.have.property( "version_name" ).that.contains( "English" );
+					version.should.have.property( "english_name" );
+					done();
+				} );
+			} );
+
+			it( "volumeLanguageFamilies should get a list of language families by media", ( done ) => {
 				let options = { media: "audio" };
-				client.volumeLanguageFamilyList( options, ( err, res ) => {
+				client.volumeLanguageFamilies( options, ( err, res ) => {
 					should.not.exist( err );
 					should.exist( res );
 					// console.log( res );
@@ -57,9 +145,9 @@ describe( "Client Specs", () => {
 				} );
 			} );
 
-			it( "volumeListing should get a list of volumes for by language family code 'ENG'", ( done ) => {
+			it( "volumes should get a list of volumes for by language family code 'ENG'", ( done ) => {
 				let options = { language_family_code: "ENG" };
-				client.volumeListing( options, ( err, res ) => {
+				client.volumes( options, ( err, res ) => {
 					should.not.exist( err );
 					should.exist( res );
 					// console.log( res );
@@ -80,9 +168,31 @@ describe( "Client Specs", () => {
 				} );
 			} );
 
-			it( "volumeListing should get a volume by dam_id", ( done ) => {
+			it( "volumes should get all volumes", ( done ) => {
+				client.volumes( ( err, res ) => {
+					should.not.exist( err );
+					should.exist( res );
+					// console.log( res );
+					res.should.be.instanceOf( Array );
+					res.length.should.be.above( 0 );
+					// console.log( res.length );
+					// 24
+					let volume = res[ 0 ];
+					// console.log( volume );
+					volume.should.have.property( "dam_id" );
+					volume.should.have.property( "fcbh_id" );
+					volume.should.have.property( "volume_name" );
+					volume.should.have.property( "language_family_code" );
+					volume.should.have.property( "language_iso" );
+					volume.should.have.property( "resolution" ).that.is.instanceOf( Array );
+					volume.should.have.property( "delivery" ).that.is.instanceOf( Array );
+					done();
+				} );
+			} );
+
+			it( "volumes should get a volume by dam_id", ( done ) => {
 				let options = { dam_id: DAM_ID };
-				client.volumeListing( options, ( err, res ) => {
+				client.volumes( options, ( err, res ) => {
 					should.not.exist( err );
 					should.exist( res );
 					// console.log( res );
@@ -103,8 +213,8 @@ describe( "Client Specs", () => {
 				} );
 			} );
 
-			it( "bookListing should get a list of books by dam_id", ( done ) => {
-				client.bookListing( DAM_ID, ( err, res ) => {
+			it( "books should get a list of books by dam_id", ( done ) => {
+				client.books( DAM_ID, ( err, res ) => {
 					should.not.exist( err );
 					should.exist( res );
 					// console.log( res );
@@ -122,8 +232,8 @@ describe( "Client Specs", () => {
 				} );
 			} );
 
-			it( "verse should get the text for entire given bible", ( done ) => {
-				client.verse( DAM_ID, ( err, res ) => {
+			it( "verses should get the text for entire given bible", ( done ) => {
+				client.verses( DAM_ID, ( err, res ) => {
 					should.not.exist( err );
 					should.exist( res );
 					// console.log( res );
@@ -142,10 +252,10 @@ describe( "Client Specs", () => {
 				} );
 			} );
 
-			it( "verse should get the text for a given book, chapter, and verse", ( done ) => {
+			it( "verses should get the text for a given book, chapter, and verse", ( done ) => {
 				should.exist( bookId );
 				const options = { book_id: bookId, chapter_id: 1, verse_start: 1, verse_end: 1 };
-				client.verse( DAM_ID, options, ( err, res ) => {
+				client.verses( DAM_ID, options, ( err, res ) => {
 					should.not.exist( err );
 					should.exist( res );
 					// console.log( res );
--- src/client.js
+++ src/client.js
@@ -26,7 +26,29 @@ export default class Client {
 		} );
 	}
 
-	volumeLanguageFamilyList( options, callback ) {
+	languages( options, callback ) {
+		const defaults = { code: "", name: "", full_word: null, family_only: null, possibilities: null, sort_by: "" };
+		if ( typeof options === "function" ) {
+			callback = options;
+			options = {};
+		}
+		const params = optionsToParams( options, defaults );
+		const path = ( params.length === 0 ) ? "library/language" : "library/language?" + params.join( "&" );
+		this._get( path, callback );
+	}
+
+	versions( options, callback ) {
+		const defaults = { code: "", name: "", sort_by: "" };
+		if ( typeof options === "function" ) {
+			callback = options;
+			options = {};
+		}
+		const params = optionsToParams( options, defaults );
+		const path = ( params.length === 0 ) ? "library/version" : "library/version?" + params.join( "&" );
+		this._get( path, callback );
+	}
+
+	volumeLanguageFamilies( options, callback ) {
 		const defaults = { media: "", root: "", full_word: false, language_code: "", delivery: "", status: "", resolution: "", organization_id: "" };
 		if ( typeof options === "function" ) {
 			callback = options;
@@ -37,7 +59,7 @@ export default class Client {
 		this._get( path, callback );
 	}
 
-	volumeListing( options, callback ) {
+	volumes( options, callback ) {
 		const defaults = { dam_id: "", fcbh_id: "", media: "", language: "", full_word: false, language_code: "",
 			language_family_code: "", updated: "", status: "live", dbp_agreement: null, expired: null, resolution: "",
 			organization_id: "", sort_by: "dam_id" };
@@ -50,12 +72,12 @@ export default class Client {
 		this._get( path, callback );
 	}
 
-	bookListing( damId, callback ) {
+	books( damId, callback ) {
 		const path = `library/book?dam_id=${damId}`;
 		this._get( path, callback );
 	}
 
-	verse( damId, options, callback ) {
+	verses( damId, options, callback ) {
 		const defaults = { book_id: "", chapter_id: "", verse_start: "", verse_end: "", markup: "" };
 		if ( typeof options === "function" ) {
 			callback = options;
