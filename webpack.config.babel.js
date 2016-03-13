fa57d5161c09a00a16034d4cb226f299ee19ee25
David Neal <david@reverentgeek.com>
2016-03-12 22:56:14 -0600

    bumping version

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,38 +1,38 @@
-import path from 'path';
+import path from "path";
 
 // globals
 
-let sourcePath = path.resolve('src');
-let outputPath = path.resolve('lib');
+let sourcePath = path.resolve( "src" );
+let outputPath = path.resolve( "lib" );
 
 // loader definitions
 
 let babelLoader = {
-  test: /\.js$/,
-  include: /src/,
-  exclude: /node_modules/,
-  loader: 'babel',
-  query: {
-    presets: ['es2015', 'stage-0']
-  }
+	test: /\.js$/,
+	include: /src/,
+	exclude: /node_modules/,
+	loader: "babel",
+	query: {
+		presets: [ "es2015", "stage-0" ]
+	}
 };
 
 // configurations
 
 let appConfiguration = {
-  context: sourcePath,
-  entry: {
-    client: './client.js'
-  },
-  output: {
-    path: outputPath,
-    filename: '[name].js'
-  },
-  module: {
-    loaders: [babelLoader]
-  }
+	context: sourcePath,
+	entry: {
+		client: "./client.js"
+	},
+	output: {
+		path: outputPath,
+		filename: "[name].js"
+	},
+	module: {
+		loaders: [ babelLoader ]
+	}
 };
 
-let configuration = [appConfiguration];
+let configuration = [ appConfiguration ];
 
 export default configuration;
